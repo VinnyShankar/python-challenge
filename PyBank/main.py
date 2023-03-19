9ddc4758ffb0a0c2a84a2e953a9a44d9571c6a0b
VinnyShankar <vinayakshank@gmail.com>
2023-03-18 17:05:00 -0700

    Notes

---
+++ PyBank/main.py
@@ -0,0 +1,17 @@
+#Read the csv file
+#Create a list using the second column (Profit/Loss list)
+#Count the number of indices in the Profit/Loss list
+
+#Create a dictionary with the headers as the keys
+
+#Sum the objects in the Profit/List list
+
+#Loop through the values and calculate the difference at each step of the loop
+
+#Create a variable to track the greatest increase in p/l
+#Create a variable to track the month with the greatest increase in p/l
+
+#Create a variable to track the greatest decrease in p/l
+#Create a variable to track the month with the greatest decrease in p/l
+
+#Write the analysis to a file (use .txt or .md etc)
